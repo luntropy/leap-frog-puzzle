b50d52e83bd9ab0c0b3afcd2f7add3f57439a3d2
Betina Borisova <b.borisowa17@gmail.com>
2020-10-22 21:32:04 +0300

    Finished version

--- State.cpp
+++ State.cpp
@@ -1,19 +1,21 @@
 #include "State.h"
 
+// Private
+
 void State::copy_objects_lake(lily* lake) {
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < N; ++i) {
         lake[i] = this->lake[i];
     }
 }
 
 void State::copy_lake_to_object(const lily* lake) {
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < N; ++i) {
         this->lake[i] = lake[i];
     }
 }
 
 bool State::compare_lakes(lily* lake) {
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < N; ++i) {
         if (this->lake[i] != lake[i]) {
             return 0;
         }
@@ -23,7 +25,7 @@ bool State::compare_lakes(lily* lake) {
 }
 
 void State::empty_lake() {
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < N; ++i) {
         this->lake[i] = EMPTY;
     }
 }
@@ -38,119 +40,7 @@ void State::clear_state() {
 void State::copy_state(const State& state) {
     this->copy_lake_to_object((&state)->lake);
     this->parent = (&state)->parent;
-    // this->copy_children(state->children);
     this->children = (&state)->children;
-}
-
-State::State() {
-    this->empty_lake();
-    this->parent = nullptr;
-}
-
-State::State(lily* lake, State* parent) : State() {
-    this->copy_lake_to_object(lake);
-    this->parent = parent;
-}
-
-State::State(const State& state) : State() {
-    this->copy_state(state);
-}
-
-State& State::operator=(const State& state) {
-    if (this != &state) {
-        this->clear_state();
-        this->copy_state(state);
-    }
-
-    return *this;
-}
-
-State::~State() {
-    this->clear_state();
-}
-
-void State::generate_states() {
-    for (int i = 0; i < 5; ++i) {
-        if (this->lake[i] == RIGHT) {
-            State* new_state = nullptr;
-            lily new_lake[5];
-            this->copy_objects_lake(new_lake);
-
-            bool generated_state = move_two_right(i, new_lake);
-
-            if (generated_state) {
-                new_state = new State(new_lake, this);
-                this->children.push_back(new_state);
-            }
-
-            this->copy_objects_lake(new_lake);
-            generated_state = move_one_right(i, new_lake);
-            if (generated_state) {
-                new_state = new State(new_lake, this);
-                this->children.push_back(new_state);
-            }
-        }
-        else if (this->lake[i] == LEFT) {
-            State* new_state = nullptr;
-            lily new_lake[5];
-            this->copy_objects_lake(new_lake);
-
-            bool generated_state = move_two_left(i, new_lake);
-
-            if (generated_state) {
-                new_state = new State(new_lake, this);
-                this->children.push_back(new_state);
-            }
-
-            this->copy_objects_lake(new_lake);
-            generated_state = move_one_left(i, new_lake);
-            if (generated_state) {
-                new_state = new State(new_lake, this);
-                this->children.push_back(new_state);
-            }
-        }
-    }
-}
-
-void State::generate_state_space() {
-    std::stack<State*> to_visit;
-    to_visit.push(this);
-
-    while (!to_visit.empty()) {
-        State* current = to_visit.top();
-        to_visit.pop();
-
-        current->generate_states();
-
-        std::vector<State*>::iterator it;
-        for (it = current->children.begin(); it != current->children.end(); ++it) {
-            to_visit.push(*it);
-        }
-    }
-}
-
-bool State::validate_movement(int index, int target_index, lily* lake) {
-    if (lake[target_index] != EMPTY || lake[index] == EMPTY || target_index < 0 || target_index > 4) {
-        return 0;
-    }
-    else if (lake[index] == RIGHT) {
-        if (index > target_index) {
-            return 0;
-        }
-        else {
-            return 1;
-        }
-    }
-    else if (lake[index] == LEFT) {
-        if (index < target_index) {
-            return 0;
-        }
-        else {
-            return 1;
-        }
-    }
-
-    return 0;
 }
 
 bool State::move_two_left(int index, lily* lake) {
@@ -205,34 +95,69 @@ bool State::move_one_right(int index, lily* lake) {
     return 0;
 }
 
-void State::print_lake() {
-        for (int i = 0; i < 5; ++i) {
-            std::cout << this->lake[i] << ' ';
-        }
-        std::cout << std::endl;
-}
-
-void State::print_children() {
-    std::vector<State*>::iterator it;
-
-    for (it = this->children.begin(); it != this->children.end(); ++it) {
-        (*it)->print_lake();
+bool State::validate_movement(int index, int target_index, lily* lake) {
+    if (lake[target_index] != EMPTY || lake[index] == EMPTY || target_index < 0 || target_index > N - 1) {
+        return 0;
     }
+    else if (lake[index] == RIGHT) {
+        if (index > target_index) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+    else if (lake[index] == LEFT) {
+        if (index < target_index) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+
+    return 0;
 }
 
-void State::print_state_space() {
-    std::stack<State*> to_visit;
-    to_visit.push(this);
+void State::generate_states() {
+    for (int i = 0; i < N; ++i) {
+        if (this->lake[i] == RIGHT) {
+            State* new_state = nullptr;
+            lily new_lake[N];
+            this->copy_objects_lake(new_lake);
 
-    while (!to_visit.empty()) {
-        State* current = to_visit.top();
-        to_visit.pop();
+            bool generated_state = move_two_right(i, new_lake);
 
-        current->print_lake();
+            if (generated_state) {
+                new_state = new State(new_lake, this);
+                this->children.push_back(new_state);
+            }
 
-        std::vector<State*>::iterator it;
-        for (it = current->children.begin(); it != current->children.end(); ++it) {
-            to_visit.push(*it);
+            this->copy_objects_lake(new_lake);
+            generated_state = move_one_right(i, new_lake);
+            if (generated_state) {
+                new_state = new State(new_lake, this);
+                this->children.push_back(new_state);
+            }
+        }
+        else if (this->lake[i] == LEFT) {
+            State* new_state = nullptr;
+            lily new_lake[N];
+            this->copy_objects_lake(new_lake);
+
+            bool generated_state = move_two_left(i, new_lake);
+
+            if (generated_state) {
+                new_state = new State(new_lake, this);
+                this->children.push_back(new_state);
+            }
+
+            this->copy_objects_lake(new_lake);
+            generated_state = move_one_left(i, new_lake);
+            if (generated_state) {
+                new_state = new State(new_lake, this);
+                this->children.push_back(new_state);
+            }
         }
     }
 }
@@ -263,6 +188,52 @@ State* State::find_result(lily* goal) {
     return nullptr;
 }
 
+// Public
+
+State::State() {
+    this->empty_lake();
+    this->parent = nullptr;
+}
+
+State::State(lily* lake, State* parent) : State() {
+    this->copy_lake_to_object(lake);
+    this->parent = parent;
+}
+
+State::State(const State& state) : State() {
+    this->copy_state(state);
+}
+
+State& State::operator=(const State& state) {
+    if (this != &state) {
+        this->clear_state();
+        this->copy_state(state);
+    }
+
+    return *this;
+}
+
+State::~State() {
+    this->clear_state();
+}
+
+void State::generate_state_space() {
+    std::stack<State*> to_visit;
+    to_visit.push(this);
+
+    while (!to_visit.empty()) {
+        State* current = to_visit.top();
+        to_visit.pop();
+
+        current->generate_states();
+
+        std::vector<State*>::iterator it;
+        for (it = current->children.begin(); it != current->children.end(); ++it) {
+            to_visit.push(*it);
+        }
+    }
+}
+
 void State::print_result_path(lily* goal) {
     State* result = this->find_result(goal);
 
@@ -280,6 +251,43 @@ void State::print_result_path(lily* goal) {
             path.pop();
 
             current->print_lake();
+        }
+    }
+}
+
+void State::print_lake() {
+        for (int i = 0; i < N; ++i) {
+            std::cout << this->lake[i] << ' ';
+        }
+        std::cout << std::endl;
+}
+
+void State::print_children() {
+    std::vector<State*>::iterator it;
+
+    for (it = this->children.begin(); it != this->children.end(); ++it) {
+        (*it)->print_lake();
+    }
+}
+
+void State::print_state_space() {
+    std::stack<State*> to_visit;
+    to_visit.push(this);
+
+    while (!to_visit.empty()) {
+        State* current = to_visit.top();
+        to_visit.pop();
+
+        std::cout << "Current parent: " << std::endl;
+        if (current->parent) {
+            (current->parent)->print_lake();
+        }
+        current->print_lake();
+        std::cout << std::endl;
+
+        std::vector<State*>::iterator it;
+        for (it = current->children.begin(); it != current->children.end(); ++it) {
+            to_visit.push(*it);
         }
     }
 }
--- State.h
+++ State.h
@@ -6,21 +6,31 @@
 #ifndef STATE_H
 #define STATE_H
 
+const int N = 5;
+
 enum lily {EMPTY, LEFT, RIGHT};
 
 class State {
 private:
-    lily lake[5];
+    lily lake[N];
     State* parent;
     std::vector<State*> children;
 
-private:
     void copy_objects_lake(lily*);
     void copy_lake_to_object(const lily*);
     bool compare_lakes(lily*);
     void empty_lake();
     void clear_state();
     void copy_state(const State&);
+
+    bool move_two_left(int, lily*);
+    bool move_one_left(int, lily*);
+    bool move_two_right(int, lily*);
+    bool move_one_right(int, lily*);
+
+    bool validate_movement(int, int, lily*);
+    void generate_states();
+    State* find_result(lily*);
 
 public:
     State();
@@ -29,20 +39,12 @@ public:
     State& operator=(const State&);
     ~State();
 
-    void generate_states();
     void generate_state_space();
-    bool validate_movement(int, int, lily*);
-    bool move_two_left(int, lily*);
-    bool move_one_left(int, lily*);
-    bool move_two_right(int, lily*);
-    bool move_one_right(int, lily*);
+    void print_result_path(lily*);
 
     void print_lake();
     void print_children();
     void print_state_space();
-
-    State* find_result(lily*);
-    void print_result_path(lily*);
 };
 
 #endif
